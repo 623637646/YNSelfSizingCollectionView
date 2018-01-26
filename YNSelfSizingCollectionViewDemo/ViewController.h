5b25e839968ae9353b792b416e1f6f30d74edb91
码猴 <mahou.wy@alibaba-inc.com>
2018-01-26 11:30:32 +0800

    no message

---
+++ YNSelfSizingCollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YNSelfSizingCollectionViewDemo
+//
+//  Created by wangya on 2018/1/26.
+//  Copyright © 2018年 Yanni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
