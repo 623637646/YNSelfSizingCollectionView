71bc35bbdeee91c1f2a238b913ae128c2a3270e5
码猴 <mahou.wy@alibaba-inc.com>
2018-01-26 11:28:04 +0800

    Initial Commit

---
+++ YNSelfSizingCollectionView/YNSelfSizingCollectionView.h
@@ -0,0 +1,19 @@
+//
+//  YNSelfSizingCollectionView.h
+//  YNSelfSizingCollectionView
+//
+//  Created by wangya on 2018/1/26.
+//  Copyright © 2018年 Yanni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YNSelfSizingCollectionView.
+FOUNDATION_EXPORT double YNSelfSizingCollectionViewVersionNumber;
+
+//! Project version string for YNSelfSizingCollectionView.
+FOUNDATION_EXPORT const unsigned char YNSelfSizingCollectionViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YNSelfSizingCollectionView/PublicHeader.h>
+
+
